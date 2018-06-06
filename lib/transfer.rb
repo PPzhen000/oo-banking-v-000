4dabb65e46863247b3f1d6c2d006ccd58e353ecf
Ariel Zhen <zhenpingping@live.com>
2018-06-06 07:47:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,14 +14,15 @@ class Transfer
   end
 
   def execute_transaction
-    if !valid?
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    else
+    if @sender.balance >= @amount
 
-      @sender.balance -= @amount && @receiver.balance += @amount
+      @receiver.deposit(@amount)
+      @sender.balance -= @amount 
 
       @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 
